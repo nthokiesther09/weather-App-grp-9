fa0f9634684e3c1e64aac0d38d0e6159b378ef19
mayangiaretha <myaretha2620@gmail.com>
2023-12-13 17:39:39 +0300

    added error handling

--- JS/index.js
+++ JS/index.js
@@ -3,6 +3,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   const input = document.querySelector('#cityInput');
   const button = document.querySelector('#Btn');
+  const weatherDataContainer = document.getElementById('weatherData');
 
 
   async function submitButton(event) {
@@ -10,18 +11,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const inputValue = input.value;
 
+    const city = inputValue;
 
     const apiKey = "597c40c39084687093b091cd48b366f8";
-    const city = inputValue;
 
     try {
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=metric`);
       const data = await response.json();
-      console.log(data,"this is the data===============>")
+      if (response.ok) {
       const iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-  
 
-      const weatherDataContainer = document.getElementById('weatherData');
       weatherDataContainer.innerHTML = `
         <h2>${data.name}, ${data.sys.country}</h2>
         <p>Temperature: ${data.main.temp}°C</p>
@@ -30,8 +29,15 @@ document.addEventListener('DOMContentLoaded', function () {
      
         
       `;
+      } else {
+        if (response.status === 404) {
+          weatherDataContainer.innerHTML = `<p>Error: City not found</p>`;
+        } else {
+          weatherDataContainer.innerHTML = `<p>Error: ${data.message}</p>`;
+        }
+      }
     } catch (error) {
-      console.error('Error fetching weather data:', error);
+      weatherDataContainer.innerHTML = `<p>Error fetching weather data: ${error.message}</p>`;
     }
   }
 
