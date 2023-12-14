ae6a2b4bd16138b8e7a86b1423d56b0b2ca67c3b
mayangiaretha <myaretha2620@gmail.com>
2023-12-14 16:02:11 +0300

    cleanup code

--- src/js/index.js
+++ src/js/index.js
@@ -5,34 +5,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
   async function submitButton(event) {
     event.preventDefault();
-
+  
     const inputValue = input.value;
-
     const apiKey = "597c40c39084687093b091cd48b366f8";
-
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=metric`;
+  
     try {
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=metric`,
-      );
+      const response = await fetch(apiUrl);
       const data = await response.json();
+  
       if (response.ok) {
-        const iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-
-        document.querySelector(".icon").src = iconUrl;
-        document.querySelector(".city").innerHTML = data.name;
-        document.querySelector(".temp").innerHTML =
-          Math.round(data.main.temp) + "℃";
-        document.querySelector(".weatherDescription").innerHTML =
-          `Weather: ${data.weather[0].description}`;
-        document.querySelector(".wind").innerHTML =
-          `Wind: ${data.wind.speed} km/h`;
+        updateWeatherUI(data);
       } else {
-        weatherDataContainer.innerHTML = `<p>Error: ${data.message}</p>`;
+        handleErrorResponse(data);
       }
     } catch (error) {
-      weatherDataContainer.innerHTML = `<p>Error fetching weather data: ${error.message}</p>`;
+      handleError(error);
     }
   }
-
+  
+  function updateWeatherUI(data) {
+    const iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  
+    document.querySelector(".icon").src = iconUrl;
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "℃";
+    document.querySelector(".weatherDescription").innerHTML = `Weather: ${data.weather[0].description}`;
+    document.querySelector(".wind").innerHTML = `Wind: ${data.wind.speed} km/h`;
+  }
+  
+  function handleErrorResponse(data) {
+    weatherDataContainer.innerHTML = `<p>Error: ${data.message}</p>`;
+  }
+  
+  function handleError(error) {
+    weatherDataContainer.innerHTML = `<p>Error fetching weather data: ${error.message}</p>`;
+  }
+  
   button.addEventListener("click", submitButton);
 });
